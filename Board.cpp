edcf509d56f7d8d48622a21397197c229d4a4a3c
yosefdassa <yosefdassa@gmail.com>
2021-03-24 20:10:43 +0200

    first commit

---
+++ Board.cpp
@@ -0,0 +1,49 @@
+// # include <iostream>
+// # include "Board.hpp"
+// # include <string>
+// using namespace std ;
+// using ariel::Board ;
+
+// namespace ariel{
+
+
+//     void Board :: post (uint row , uint colum , Direction direction , string message_post){
+
+
+//     }
+
+//     string Board :: read(uint row  , uint colum , Direction direction , uint char_size){
+   
+//         return "text";
+//     }   
+
+//     void Board :: show(){
+
+//     }
+// }
+
+
+
+#include <iostream>
+#include "Board.hpp"
+
+namespace ariel
+{
+    void Board::post(uint row, uint colum, ariel::Direction direction, string message_post)
+    {
+    }
+
+    string Board::read(uint row, uint colum, ariel::Direction direction, uint char_size)
+    {
+
+        return "text";
+    }
+
+    void Board::show()
+    {
+    }
+    Board :: Board(){}
+    Board ::~Board(){}
+    
+
+}
